e80c6f487d1ee6eba5ecc2221cc3b86133358206
backlash <xixianbing@hexiaoxiang.com>
2021-05-20 18:58:42 +0800

    bubble order

---
+++ algorithm/bubble.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const (
+	LENGTH = 6
+)
+
+func main() {
+	number := []int{6, 5, 2, 3, 4, 1}
+	bubbleSort(number)
+
+}
+
+func bubbleSort(nums []int) {
+	flags1 := false
+	for i := 0; i < len(nums); i++ {
+
+		for j := 1; j < len(nums)-i; j++ {
+			if nums[j-1] > nums[j] {
+				//交换
+				nums[j], nums[j-1] = nums[j-1], nums[j]
+				flags1 = true
+			}
+		}
+		if !flags1 {
+			break
+		}
+	}
+	fmt.Println(nums)
+}
